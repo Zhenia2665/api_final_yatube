9abc874d8e65b518d42b815bb4001b519f38170d
Evgenia Tarasova <etarasova@live.ru>
2023-03-12 14:52:39 -0700

    Added mixins and generics

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework import viewsets
+from rest_framework import generics, mixins, status, viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
-
-from posts.models import Group, Post
+from rest_framework.response import Response
 
 from api.permissions import IsAuthorOrReadOnly
 from api.serializers import (
@@ -15,6 +14,8 @@ from api.serializers import (
     GroupSerializer,
     PostSerializer,
 )
+
+from posts.models import Group, Post, Follow, User
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -26,6 +27,17 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ("group",)
+
+    def create(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED,
+                            headers=headers)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -53,17 +65,36 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user, post=post)
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    generics.GenericAPIView):
     """Вьюсет для обработки подписок."""
-
+    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     filter_backends = (SearchFilter,)
     search_fields = ("user__username", "following__username")
     permission_classes = (IsAuthenticated,)
 
-    def get_queryset(self):
+    def get(self, request, *args, **kwargs):
         user = self.request.user
-        return user.follower.all()
+        queryset = user.follower.all()
+        follower = self.request.query_params.get('search', None)
+        print(follower)
+        if follower is not None:
+            user_2 = User.objects.get(username=follower)
+            queryset = queryset.filter(following=user_2)
+        # return queryset
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
